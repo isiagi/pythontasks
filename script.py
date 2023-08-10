9b480772cf9e6000a377c0869b6c48bc72ca3b25
isiagi <isiagigeofrey0@gmail.com>
2023-08-10 13:40:40 +0300

    add list assignment partly

--- script.py
+++ script.py
@@ -1,163 +1,201 @@
 # 1 Create a function named print_numbers().
 
-def print_numbers():
-    print(5)
-    print(100)
+# def print_numbers():
+#     print(5)
+#     print(100)
 
-print_numbers()
-print_numbers()
+# print_numbers()
+# print_numbers()
 
 
 # 2 Write a program to check if the student passed or failed his/her examination by creating a function..
 
-user_input = input("Enter Marks Please: ")
+# user_input = input("Enter Marks Please: ")
 
-def checkMarks(user_inputz):
+# def checkMarks(user_inputz):
     
-    try:
-        int_marks = int(user_inputz)
-    except:
-        print("Unable to convert user input to integer")
-        return
+#     try:
+#         int_marks = int(user_inputz)
+#     except:
+#         print("Unable to convert user input to integer")
+#         return
         
-    if (int_marks >= 70):
-        print("Pass")
-    else:
-        print("Fail")
+#     if (int_marks >= 70):
+#         print("Pass")
+#     else:
+#         print("Fail")
 
-checkMarks(user_input)
+# checkMarks(user_input)
 
 # 3. Write a program to check if the two arguments passed are equal or not by creating a function.
 
-def my_function(num1, num2):
-    if (num1 == num2):
-        print(True)
-    else:
-        print(False)
+# def my_function(num1, num2):
+#     if (num1 == num2):
+#         print(True)
+#     else:
+#         print(False)
 
-my_function(1, "1")
+# my_function(1, "1")
 
 # 4. Write a program to print full name with space in between using a function.
 
-def full_name(first_name, last_name):
-    if (isinstance(first_name, str) and isinstance(last_name, str)):
-        print(first_name + " " + last_name)
-        return
+# def full_name(first_name, last_name):
+#     if (isinstance(first_name, str) and isinstance(last_name, str)):
+#         print(first_name + " " + last_name)
+#         return
         
-    print("Inputs must be strings!")
-    return
+#     print("Inputs must be strings!")
+#     return
 
-full_name("Geofrey", 90)
+# full_name("Geofrey", 90)
     
 # 5. Write a program to check whether a number is prime or not using a function.
 
-def check_prime(num):
-    flag = False
+# def check_prime(num):
+#     flag = False
 
-    if(num == 1):
-        print("1 is not a prime number")
-    elif num > 1:
-        for i in range(2, num + 1):
-            print(i)
-            if (num % i ) == 0:
-                flag = True
-                break
+#     if(num == 1):
+#         print("1 is not a prime number")
+#     elif num > 1:
+#         for i in range(2, num + 1):
+#             print(i)
+#             if (num % i ) == 0:
+#                 flag = True
+#                 break
         
-    if flag:
-        print(num, "Is not prime")
-    else:
-        print(num, "Is prime number")
+#     if flag:
+#         print(num, "Is not prime")
+#     else:
+#         print(num, "Is prime number")
                 
         
 
-check_prime(6)
+# check_prime(6)
 
 
 # 6. Write a program to calculate the power of an integer using a function.
 
-num = input("Enter Number: ")
+# num = input("Enter Number: ")
 
-flag = False
+# flag = False
 
-try:
-    num_marks = int(num)
-except:
-    flag = True
+# try:
+#     num_marks = int(num)
+# except:
+#     flag = True
 
-def calculate_pow(arg1):
-    print(arg1 ** 3)
+# def calculate_pow(arg1):
+#     print(arg1 ** 3)
 
-if flag:
-    print("Unable to convert user input to integer")
-else:
-    calculate_pow(num_marks)
+# if flag:
+#     print("Unable to convert user input to integer")
+# else:
+#     calculate_pow(num_marks)
 
 
 # 7. Write a program to print a person's information.
 
-country = input("Please enter country!")
+# country = input("Please enter country!")
 
-def display_info(name, location):
-    print(f"{name}\n{location}")
+# def display_info(name, location):
+#     print(f"{name}\n{location}")
 
-display_info("Magnus", country)
+# display_info("Magnus", country)
 
 
 # 8. Write a program to find the sum of N natural numbers by creating a function.
 
-def find_sum(n):
-    count = 0
-    for i in range(1,n + 1):
-        print("===count===", count)
-        print("===n===", i)
-        count += i
-        print("===countnew===", count)
-    else:
-        print(count)
+# def find_sum(n):
+#     count = 0
+#     for i in range(1,n + 1):
+#         print("===count===", count)
+#         print("===n===", i)
+#         count += i
+#         print("===countnew===", count)
+#     else:
+#         print(count)
 
-find_sum(5)
+# find_sum(5)
 
 
 # 9. Write a program to compute the area of a circle using a function.
 
-pi_value = 3.14
+# pi_value = 3.14
 
-def compute_area(radius, pi):
-    area = pi * radius**2
+# def compute_area(radius, pi):
+#     area = pi * radius**2
 
-    print(area)
+#     print(area)
 
-compute_area(10, pi_value)
+# compute_area(10, pi_value)
 
 
 # 10. Create a function named call_me() that takes two arguments a and b.
 
-import sys
+# import sys
 
-n = input("Enter Integer Number: ")
+# n = input("Enter Integer Number: ")
 
-try:
-    num = int(n)
-except:
-    sys.exit("Input can't be convert to  number")
+# try:
+#     num = int(n)
+# except:
+#     sys.exit("Input can't be convert to  number")
     
 
-def call_me(a = 5, b = 10):
+# def call_me(a = 5, b = 10):
 
-    print(a)
-    print(b)
+#     print(a)
+#     print(b)
 
-call_me(num)
+# call_me(num)
 
 
 # 11. Create a function named print_items() that can take any number of arguments.
 
-text1 = "hello"
-text2 = "world"
+# text1 = "hello"
+# text2 = "world"
 
-def print_items(*argv):
-    for arg in argv:
-        print(arg)
+# def print_items(*argv):
+#     for arg in argv:
+#         print(arg)
 
-print_items(text1)
-print_items(text1, text2)
+# print_items(text1)
+# print_items(text1, text2)
+
+
+
+#lists
+
+# 1. Write a program to print individual items of a list.
+
+fruits = ["apple", "kiwi", "grapes", "mango"]
+
+for i in fruits:
+    print(i)
+
+
+# 2. Write a program to add an item (entered by the user) to the end of the list.
+num = int(input("Please enter number: "))
+
+odd = [1,3,5]
+
+def add(num):
+    odd.append(num)
+    print(odd)
+
+add(num)
+
+
+# 3. Write a program to calculate the product of list items.
+
+my_list = [2, 5, 3, 4, 1]
+
+def mulit(arr):
+    count = 1
+
+    for i in arr:
+        count *= i
+
+    print(count)
+
+mulit(my_list)
